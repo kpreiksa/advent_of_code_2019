d37f2acf2b314c7e6734cb63618fec73a4e1d64a
Kyle Preiksa <preiksa.kyle@gmail.com>
2019-12-08 09:04:17 -0500

    refactored part1 again to be more consistent with part 2

--- day2/part1.py
+++ day2/part1.py
@@ -1,9 +1,8 @@
 def run_instruction(ip, ints):
     # returns length of instruction executed, or -1 to halt program execution
     inst = ints[ip]
-    print(inst)
     if (inst == 1):
-        print('Add')
+        # print('Add')
         param1 = ints[ip + 1]
         param2 = ints[ip + 2]
         param3 = ints[ip + 3]
@@ -12,7 +11,7 @@ def run_instruction(ip, ints):
 
     elif (inst == 2):
         # multiply
-        print('Multiply')
+        # print('Multiply')
         param1 = ints[ip + 1]
         param2 = ints[ip + 2]
         param3 = ints[ip + 3]
@@ -20,23 +19,28 @@ def run_instruction(ip, ints):
         return 4
     elif (inst == 99):
         # halt
-        print('Halt')
+        # print('Halt')
         return -1
+    else:
+        print(f'Invalid instruction: {inst}')
+
+def load_memory(file):
+    f = open(file)
+    line = f.readlines()[0].strip()
+    ints_str = line.split(',')
+    ints = [int(x) for x in ints_str]
+    return ints
 
 
-f = open('input.txt')
-line = f.readlines()[0].strip()
-ints_str = line.split(',')
-ints = [int(x) for x in ints_str]
+ints = load_memory('input.txt')
+ints[1] = 12
+ints[2] = 2
 pc = 0 # program counter
 while(pc < len(ints)):
-    print(f'Program Counter: {pc}')
+    # print(f'Program Counter: {pc}')
     return_value = run_instruction(pc, ints)
     if (return_value == -1):
         break
     else:
         pc += return_value
-print('Done.')
-
-
-
+print(ints[0])
