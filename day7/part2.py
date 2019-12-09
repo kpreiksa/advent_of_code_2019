ceaf69c14f2529caebbca93cb2c6641c1bc5dcd3
Kyle Preiksa <preiksa.kyle@gmail.com>
2019-12-08 19:35:06 -0500

    day 7 parts 1 and 2

--- day7/part2.py
+++ day7/part2.py
@@ -1,26 +1,48 @@
 
 from itertools import permutations 
+import time
 
 
 class IntCodeComputer():
-    def __init__(self, phase):
-        self.output = 0
+    def __init__(self, phase, input_delegate, initial_input_value = None):
+        self._output = 0
+        self._output_available = False
         self.ints = self.load_memory('input.txt')
         self.pc = 0 # program counter
         self._input_instruction = 0
         self._phase = phase
+        self._input_value = initial_input_value
+        self._input_valid = False
+        self.is_executing = False
+        self._input_delegate = input_delegate
+        self._execution_paused = False
 
-    def set_input(self, input_value):
-        self._input_value = input_value
+    @property
+    def output(self):
+        self._output_available = False
+        return self._output
+
+    @property
+    def output_available(self):
+        return self._output_available
+
+    def set_input_delegate(self, input_delegate):
+        self._input_delegate = input_delegate
+        print('New input provided')
 
     def execute(self):
-            while(self.pc < len(self.ints)):
-                # print(f'Program Counter: {pc}')
-                return_value = self.run_instruction(self.pc, self.ints)
-                if (return_value == -1):
-                    return -1
-                else:
-                    self.pc = return_value
+        self.is_executing = True
+        while(self.pc < len(self.ints)):
+            # print(f'Program Counter: {pc}')
+            return_value = self.run_instruction(self.pc, self.ints)
+            if (return_value == -1):
+                return -1
+            elif (return_value == -2):
+                self._execution_paused = True
+                return -2
+            else:
+                self.pc = return_value
+        return 0
 
 
     def run_instruction(self, ip, ints):
@@ -79,10 +101,11 @@ class IntCodeComputer():
             if (self._input_instruction == 0):
                 input_value = self._phase
             elif (self._input_instruction >= 1):
-                input_value = self._input_value
+                input_value = self._input_delegate()
+                if(input_value == None):
+                    return -2
             # input_value = input('Enter input')
             self._input_instruction += 1
-            print(f'Input # {self._input_instruction}')
             ints[ints[ip+1]] = int(input_value)
             return ip+2
 
@@ -92,8 +115,9 @@ class IntCodeComputer():
                 test_result = ints[ints[ip+1]]
             elif param1_mode == 1:
                 test_result = ints[ip+1]
-            self.output = test_result
-            # print(f'Test: {test_result}')
+            self._output = test_result
+            self._output_available = True
+            print(f'Test: {test_result}')
             return ip+2
 
         elif (opcode == 5): # jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
@@ -191,29 +215,87 @@ class IntCodeComputer():
 
 amp_settings = list(permutations(range(5, 10)))
 
+global ampA_ready, ampB_ready, ampC_ready, ampD_ready, ampE_ready
+global ampE_called_prior
+global ampA, ampB, ampC, ampD, ampE
+
+ampE_called_prior = False
+ampA_ready = False
+ampB_ready = False
+ampC_ready = False
+ampD_ready = False
+ampE_ready = False
+
+def getOutputA():
+    global ampA, ampA_ready
+
+    if(ampA_ready):
+        ampA_ready = False
+        return ampA.output
+    else:
+        return None
+
+def getOutputB():
+    global ampB, ampB_ready
+    if(ampB_ready):
+        ampB_ready = False
+        return ampB.output
+    else:
+        return None
+
+def getOutputC():
+    global ampC, ampC_ready
+    if(ampC_ready):
+        ampC_ready = False
+        return ampC.output
+    else:
+        return None
+
+def getOutputD():
+    global ampD, ampD_ready
+    if(ampD_ready):
+        ampD_ready = False
+        return ampD.output
+    else:
+        return None
+
+def getOutputE():
+    global ampE, ampE_ready
+    global ampE_called_prior
+    if (ampE_called_prior):
+        ampE_called_prior = True
+        if(ampE_ready):
+            ampE_ready = False
+            return ampE.output
+        else:
+            return None
+    else:
+        ampE_called_prior = True
+        return 0
+
+
 outputs = []
 for amp_setting in amp_settings:
 
-    ampA = IntCodeComputer(amp_setting[0])
-    ampB = IntCodeComputer(amp_setting[1])
-    ampC = IntCodeComputer(amp_setting[2])
-    ampD = IntCodeComputer(amp_setting[3])
-    ampE = IntCodeComputer(amp_setting[4])
-
-    ampA_input = 0
-    return_value = 0
-    while return_value != -1:
-        ampA.set_input(ampA_input)
-        return_value |= ampA.execute()
-        ampB.set_input(ampA.output)
-        return_value |= ampB.execute()
-        ampC.set_input(ampB.output)
-        return_value |= ampC.execute()
-        ampD.set_input(ampC.output)
-        return_value |= ampD.execute()
-        ampE.set_input(ampD.output)
-        return_value |= ampE.execute()
-        ampA_input = ampE.output
+    ampA = IntCodeComputer(amp_setting[0], getOutputE)
+    ampB = IntCodeComputer(amp_setting[1], getOutputA)
+    ampC = IntCodeComputer(amp_setting[2], getOutputB)
+    ampD = IntCodeComputer(amp_setting[3], getOutputC)
+    ampE = IntCodeComputer(amp_setting[4], getOutputD)
+    retcode = 0
+    while(retcode != -1):
+        # print('Running')
+        retcode = ampA.execute()
+        retcode = ampB.execute()
+        retcode = ampC.execute()
+        retcode = ampD.execute()
+        retcode = ampE.execute()
+        ampA_ready = ampA.output_available
+        ampB_ready = ampB.output_available
+        ampC_ready = ampC.output_available
+        ampD_ready = ampD.output_available
+        ampE_ready = ampE.output_available
+        # print(f'A:{ampA_ready} B:{ampB_ready} C:{ampC_ready} D:{ampD_ready} E:{ampE_ready}')
 
     outputs.append(ampE.output)
     
