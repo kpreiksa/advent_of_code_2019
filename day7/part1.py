3cdc092e78ba2a3dbc7c06e153869346bca80fec
Kyle Preiksa <preiksa.kyle@gmail.com>
2019-12-09 22:07:27 -0500

    committing before making another huge change to IntCodeComputer

--- day7/part1.py
+++ day7/part1.py
@@ -1,5 +1,59 @@
 
-from itertools import permutations 
+from itertools import permutations
+
+class ProgramTerminatedError(Exception):
+    pass
+
+class Opcode():
+    def __init__(self, ints, pc):
+        ''' Starting with an integer, generate the opcode obj '''
+        self._raw_opcode = ints[pc]
+        self._address = pc
+        self._inst_string = f'{self._raw_opcode:#05}' # 5 = max number of digits for an instruction
+        
+    @property
+    def opcode(self):
+        return int(self._inst_string[-2:])
+
+    @property
+    def param1_addr(self):
+        return self._address+1
+
+    @property
+    def param2_addr(self):
+        return self._address+2
+
+    @property
+    def param3_addr(self):
+        return self._address+3
+
+    @property
+    def param1_value(self, mem):
+        if (self.param1_mode == 0):
+            return mem[mem[self.param1_addr]]
+
+        elif (self.param1_mode == 1): 
+            return mem[self.param1_addr]
+
+    @property
+    def param2_value(self, mem):
+        if (self.param2_mode == 0):
+            return mem[mem[self.param2_addr]]
+
+        elif (self.param2_mode == 1): 
+            return mem[self.param2_addr]
+
+    @property
+    def param1_mode(self):
+        return int(self._inst_string[-3])
+
+    @property
+    def param2_mode(self):
+        return int(self._inst_string[-4])
+
+    @property
+    def address(self):
+        return self._address
 
 
 class IntCodeComputer():
@@ -10,172 +64,162 @@ class IntCodeComputer():
         self._input_instruction = 0
         self._phase = phase
         self._input_value = input_value
+        self._opcodes = {
+            1: self.oc_add,
+            2: self.oc_mult,
+            3: self.oc_input,
+            4: self.oc_output,
+            5: self.oc_jmpT,
+            6: self.oc_jmpF,
+            7: self.oc_lt,
+            8: self.oc_eq,
+            99:self.oc_halt,
+        }
+
+    def oc_add(self, oc):
+
+        param1 = oc.param1_value
+        if (oc.param1_mode == 0): # 0 is position mode
+            param1 = self.ints[self.ints[self.pc + 1]]
+        elif (oc.param1_mode == 1): # 1 is immediate mode
+            param1 = self.ints[self.pc + 1]
+
+
+        if (oc.param2_mode == 0): # 0 is position mode
+            param2 = self.ints[self.ints[self.pc + 2]]
+        elif (oc.param2_mode == 1): # 1 is immediate mode
+            param2 = self.ints[self.pc + 2]
+
+        param3 = self.ints[self.pc + 3] # always position mode
+        self.ints[param3] = param1 + param2
+        self.pc += 4
+
+    def oc_mult(self, oc):
+
+        if (oc.param1_mode == 0): # 0 is position mode
+            param1 = self.ints[self.ints[self.pc + 1]] # dereference
+        elif (oc.param1_mode == 1): # 1 is immediate mode
+            param1 = self.ints[self.pc + 1]
+
+        if (oc.param2_mode == 0): # 0 is position mode
+            param2 = self.ints[self.ints[self.pc + 2]]
+        elif (oc.param2_mode == 1): # 1 is immediate mode
+            param2 = self.ints[self.pc + 2]
+
+        param3 = self.ints[self.pc + 3] # always position mode
+        self.ints[param3] = param1 * param2
+        self.pc += 4
+
+
+    def oc_input(self, oc):
+
+        if (self._input_instruction == 0):
+            input_value = self._phase
+        elif (self._input_instruction == 1):
+            input_value = self._input_value
+        # input_value = input('Enter input')
+        self._input_instruction += 1
+        self.ints[self.ints[self.pc+1]] = int(input_value)
+        self.pc += 2
+
+    def oc_output(self, oc):
+        if oc.param1_mode == 0:
+            test_result = self.ints[self.ints[self.pc+1]]
+        elif oc.param1_mode == 1:
+            test_result = self.ints[self.pc+1]
+        self.output = test_result
+        # print(f'Test: {test_result}')
+        self.pc += 2
+
+    def oc_jmpT(self, oc):
+        if (oc.param1_mode == 0): # 0 is position mode
+            param1 = self.ints[self.ints[self.pc + 1]] # dereference
+        elif (oc.param1_mode == 1): # 1 is immediate mode
+            param1 = self.ints[self.pc + 1]
+
+        if (oc.param2_mode == 0): # 0 is position mode
+            param2 = self.ints[self.ints[self.pc + 2]]
+        elif (oc.param2_mode == 1): # 1 is immediate mode
+            param2 = self.ints[self.pc + 2]
+
+        if param1 != 0:
+            self.pc = param2
+        else:
+            self.pc += 2
+
+    def oc_jmpF(self, oc):
+        if (oc.param1_mode == 0): # 0 is position mode
+            param1 = self.ints[self.ints[self.pc + 1]] # dereference
+        elif (oc.param1_mode == 1): # 1 is immediate mode
+            param1 = self.ints[self.pc + 1]
+
+        if (oc.param2_mode == 0): # 0 is position mode
+            param2 = self.ints[self.ints[self.pc + 2]]
+        elif (oc.param2_mode == 1): # 1 is immediate mode
+            param2 = self.ints[self.pc + 2]
+
+        if param1 == 0:
+            self.pc = param2
+        else:
+            self.pc += 3
+
+    def oc_lt(self, oc):
+        if (oc.param1_mode == 0): # 0 is position mode
+            param1 = self.ints[self.ints[self.pc + 1]] # dereference
+        elif (oc.param1_mode == 1): # 1 is immediate mode
+            param1 = self.ints[self.pc + 1]
+
+        if (oc.param2_mode == 0): # 0 is position mode
+            param2 = self.ints[self.ints[self.pc + 2]]
+        elif (oc.param2_mode == 1): # 1 is immediate mode
+            param2 = self.ints[self.pc + 2]
+
+        param3 = self.ints[self.pc+3]
+
+        if param1 < param2:
+            self. ints[param3] = 1
+        else:
+            self.ints[param3] = 0
+        
+        self.pc += 4
+
+    def oc_eq(self, oc):
+        if (oc.param1_mode == 0): # 0 is position mode
+            param1 = self.ints[self.ints[self.pc + 1]] # dereference
+        elif (oc.param1_mode == 1): # 1 is immediate mode
+            param1 = self.ints[self.pc + 1]
+
+        if (oc.param2_mode == 0): # 0 is position mode
+            param2 = self.ints[self.ints[self.pc + 2]]
+        elif (oc.param2_mode == 1): # 1 is immediate mode
+            param2 = self.ints[self.pc + 2]
+
+        param3 = self.ints[self.pc+3]
+
+        if param1 == param2:
+            self.ints[param3] = 1
+        else:
+            self.ints[param3] = 0
+        
+        self.pc += 4
+
+    def oc_halt(self, oc):
+        raise ProgramTerminatedError()
+
+
+    def run_instruction(self):
+        # returns length of instruction executed, or -1 to halt program execution
+        oc = Opcode(self.ints, self.pc)
+        self._opcodes[oc.opcode](oc)
+
+    def run_to_end(self):
         while(self.pc < len(self.ints)):
             # print(f'Program Counter: {pc}')
-            return_value = self.run_instruction(self.pc, self.ints)
-            if (return_value == -1):
+            try:
+                self.run_instruction()
+            except ProgramTerminatedError:
                 break
-            else:
-                self.pc = return_value
 
-
-    def run_instruction(self, ip, ints):
-        # returns length of instruction executed, or -1 to halt program execution
-        inst = ints[ip]
-        inst_string = f'{inst:#05}' # 5 = max number of digits for an instruction
-        opcode = int(inst_string[-2:])
-        if (opcode == 1): # addition. 3 parameters
-            # print('Add')
-            param1_mode = int(inst_string[-3])
-            param2_mode = int(inst_string[-4])
-            inst_str = 'Add'
-            # param3_mode = inst_string[-5] Don't need this since it's the output
-
-            if (param1_mode == 0): # 0 is position mode
-                param1 = ints[ints[ip + 1]]
-                inst_str += f' ({ints[ip +1]}) -> {ints[ints[ip+1]]}'
-            elif (param1_mode == 1): # 1 is immediate mode
-                param1 = ints[ip + 1]
-                inst_str += f' {ints[ip+1]}'
-
-            if (param2_mode == 0): # 0 is position mode
-                param2 = ints[ints[ip + 2]]
-                inst_str += f' + ({ints[ip +2]}) -> {ints[ints[ip+2]]}'
-            elif (param2_mode == 1): # 1 is immediate mode
-                param2 = ints[ip + 2]
-                inst_str += f' + {ints[ip+2]}'
-
-            param3 = ints[ip + 3] # always position mode
-            inst_str += f' => ({param3})'
-            ints[param3] = param1 + param2
-            return ip+4
-
-        elif (opcode == 2): # multiplication. 3 parameters
-            # multiply
-            # print('Multiply')
-            param1_mode = int(inst_string[-3])
-            param2_mode = int(inst_string[-4])
-            # param3_mode = inst_string[-5] Don't need this since it's the output
-
-            if (param1_mode == 0): # 0 is position mode
-                param1 = ints[ints[ip + 1]] # dereference
-            elif (param1_mode == 1): # 1 is immediate mode
-                param1 = ints[ip + 1]
-
-            if (param2_mode == 0): # 0 is position mode
-                param2 = ints[ints[ip + 2]]
-            elif (param2_mode == 1): # 1 is immediate mode
-                param2 = ints[ip + 2]
-
-            param3 = ints[ip + 3] # always position mode
-            ints[param3] = param1 * param2
-            return ip+4
-
-        elif (opcode == 3): #input
-            if (self._input_instruction == 0):
-                input_value = self._phase
-            elif (self._input_instruction == 1):
-                input_value = self._input_value
-            # input_value = input('Enter input')
-            self._input_instruction += 1
-            ints[ints[ip+1]] = int(input_value)
-            return ip+2
-
-        elif (opcode == 4): #output
-            param1_mode = int(inst_string[-3])
-            if param1_mode == 0:
-                test_result = ints[ints[ip+1]]
-            elif param1_mode == 1:
-                test_result = ints[ip+1]
-            self.output = test_result
-            # print(f'Test: {test_result}')
-            return ip+2
-
-        elif (opcode == 5): # jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-            param1_mode = int(inst_string[-3])
-            param2_mode = int(inst_string[-4])
-            if (param1_mode == 0): # 0 is position mode
-                param1 = ints[ints[ip + 1]] # dereference
-            elif (param1_mode == 1): # 1 is immediate mode
-                param1 = ints[ip + 1]
-
-            if (param2_mode == 0): # 0 is position mode
-                param2 = ints[ints[ip + 2]]
-            elif (param2_mode == 1): # 1 is immediate mode
-                param2 = ints[ip + 2]
-
-            if param1 != 0:
-                return param2
-            else:
-                return ip+3
-
-        elif (opcode == 6): # jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-            param1_mode = int(inst_string[-3])
-            param2_mode = int(inst_string[-4])
-            if (param1_mode == 0): # 0 is position mode
-                param1 = ints[ints[ip + 1]] # dereference
-            elif (param1_mode == 1): # 1 is immediate mode
-                param1 = ints[ip + 1]
-
-            if (param2_mode == 0): # 0 is position mode
-                param2 = ints[ints[ip + 2]]
-            elif (param2_mode == 1): # 1 is immediate mode
-                param2 = ints[ip + 2]
-
-            if param1 == 0:
-                return param2
-            else:
-                return ip+3
-
-        elif (opcode == 7): # less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-            param1_mode = int(inst_string[-3])
-            param2_mode = int(inst_string[-4])
-            if (param1_mode == 0): # 0 is position mode
-                param1 = ints[ints[ip + 1]] # dereference
-            elif (param1_mode == 1): # 1 is immediate mode
-                param1 = ints[ip + 1]
-
-            if (param2_mode == 0): # 0 is position mode
-                param2 = ints[ints[ip + 2]]
-            elif (param2_mode == 1): # 1 is immediate mode
-                param2 = ints[ip + 2]
-
-            param3 = ints[ip+3]
-
-            if param1 < param2:
-                ints[param3] = 1
-            else:
-                ints[param3] = 0
-            
-            return ip + 4
-
-        elif (opcode == 8): # equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-            param1_mode = int(inst_string[-3])
-            param2_mode = int(inst_string[-4])
-            if (param1_mode == 0): # 0 is position mode
-                param1 = ints[ints[ip + 1]] # dereference
-            elif (param1_mode == 1): # 1 is immediate mode
-                param1 = ints[ip + 1]
-
-            if (param2_mode == 0): # 0 is position mode
-                param2 = ints[ints[ip + 2]]
-            elif (param2_mode == 1): # 1 is immediate mode
-                param2 = ints[ip + 2]
-
-            param3 = ints[ip+3]
-
-            if param1 == param2:
-                ints[param3] = 1
-            else:
-                ints[param3] = 0
-            
-            return ip + 4
-        elif (opcode == 99):
-            # halt
-            # print('Halt')
-            return -1
-        else:
-            print(f'Invalid instruction: {inst}')
 
     def load_memory(self, file):
         f = open(file)
@@ -190,10 +234,15 @@ outputs = []
 for amp_setting in amp_settings:
 
     ampA = IntCodeComputer(amp_setting[0], 0)
+    ampA.run_to_end()
     ampB = IntCodeComputer(amp_setting[1], ampA.output)
+    ampB.run_to_end()
     ampC = IntCodeComputer(amp_setting[2], ampB.output)
+    ampC.run_to_end()
     ampD = IntCodeComputer(amp_setting[3], ampC.output)
+    ampD.run_to_end()
     ampE = IntCodeComputer(amp_setting[4], ampD.output)
+    ampE.run_to_end()
     outputs.append(ampE.output)
     
 max_output = [0,0]
